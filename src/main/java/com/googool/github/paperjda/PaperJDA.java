9cedb254b2e06cb36194993d4674998ceac6ac7a
Ask <askx0113@gmail.com>
2022-09-30 23:18:48 +0200

    Update PaperJDA.java

--- src/main/java/com/googool/github/paperjda/PaperJDA.java
+++ src/main/java/com/googool/github/paperjda/PaperJDA.java
@@ -5,22 +5,22 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperJDA extends JavaPlugin {
+public class PaperJDA extends JavaPlugin {
 
     public JDA jda;
 
     @Override
     public void onEnable() {
-        String DISCORD_TOKEN = getConfig().getString("DISCORD_TOKEN");
+        String DISCORD_TOKEN = getConfig().getString("DISCORD_TOKEN"); // Config.yml.
         if (DISCORD_TOKEN == null) {
             this.getPluginLoader().disablePlugin(this);
             this.getLogger().severe("Please provide a DISCORD_TOKEN in the config.yml file.");
-            return;
+            return; // If no token is provided the plugin will disable itself.
         }
 
         try {
             this.jda = JDABuilder.createDefault(DISCORD_TOKEN)
-                    .addEventListeners(new CommandManager())
+                    .addEventListeners(new CommandManager()) // Collection of commands.
                     .build().awaitReady();
 
         } catch (Exception exception) {
