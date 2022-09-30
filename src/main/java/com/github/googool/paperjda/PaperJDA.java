a6c292383c8e33304cc0f7f01cc9070ff1620f9e
Ask <askx0113@gmail.com>
2022-09-30 22:31:54 +0200

    Update PaperJDA.java

--- src/main/java/com/github/googool/paperjda/PaperJDA.java
+++ src/main/java/com/github/googool/paperjda/PaperJDA.java
@@ -1,48 +1,29 @@
-package com.github.googool.paperjda;
+package com.googool.github.paperjda;
 
-import com.github.googool.paperjda.Commands.DiscordCommands;
-import com.github.googool.paperjda.Listeners.DiscordListener;
+import com.googool.github.paperjda.Commands.CommandManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Guild;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import javax.security.auth.login.LoginException;
-
 public final class PaperJDA extends JavaPlugin {
-    private JDA jda;
+
+    public JDA jda;
 
     @Override
     public void onEnable() {
         String DISCORD_TOKEN = getConfig().getString("DISCORD_TOKEN");
-        String DISCORD_GUILD = getConfig().getString("DISCORD_GUILD");
-
         if (DISCORD_TOKEN == null) {
             this.getPluginLoader().disablePlugin(this);
             this.getLogger().severe("Please provide a DISCORD_TOKEN in the config.yml file.");
-        } else if (DISCORD_GUILD == null) {
-            this.getPluginLoader().disablePlugin(this);
-            this.getLogger().severe("Please provide a DISCORD_GUILD in the config.yml file.");
+            return;
         }
 
         try {
             this.jda = JDABuilder.createDefault(DISCORD_TOKEN)
-                    .setStatus(OnlineStatus.ONLINE)
-                    .setActivity(Activity.playing("Minecraft"))
-                    .addEventListeners(new DiscordCommands(), new DiscordListener())
-                    .build()
-                    .awaitReady();
+                    .addEventListeners(new CommandManager())
+                    .build().awaitReady();
 
-            Guild guild = this.jda.getGuildById(DISCORD_GUILD);
-
-            if (guild != null) {
-                this.jda.getGuildById(DISCORD_GUILD)
-                        .upsertCommand("fart", "Fart really hard").queue();
-            }
-
-        } catch (LoginException | InterruptedException exception) {
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
