18a4642ed572ac5e05b9c3ed387eacc45cf1f4db
Ask <askx0113@gmail.com>
2022-09-30 23:19:29 +0200

    Update CommandManager.java

--- src/main/java/com/googool/github/paperjda/Commands/CommandManager.java
+++ src/main/java/com/googool/github/paperjda/Commands/CommandManager.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 /**
  * Add new EventListeners inside the CommandManager to prevent clocking up the Main.
+ * onReady runs when the application is ready.
  */
 
 public class CommandManager extends ListenerAdapter {
@@ -19,12 +20,12 @@ public class CommandManager extends ListenerAdapter {
     @Override
     public void onReady(@NotNull ReadyEvent event) {
 
-        // Add your commands here.
+        // Add ListenerAdapters here to register.
         event.getJDA().addEventListener(
                 new Ping()
         );
 
-        // Register your commands here.
+        // Register and give your commands a name and description.
         List<CommandData> commandData = new ArrayList<>();
         commandData.add(Commands.slash("ping", "Pong! 🏓"));
         event.getJDA().updateCommands().addCommands(commandData).queue();
