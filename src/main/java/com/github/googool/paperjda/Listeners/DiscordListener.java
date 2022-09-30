a78012ea2a64668ddb4855d91a4b606368babac8
Ask <askx0113@gmail.com>
2022-09-30 22:37:44 +0200

    Delete src/main directory

--- src/main/java/com/github/googool/paperjda/Listeners/DiscordListener.java
+++
@@ -1,7 +0,0 @@
-package com.github.googool.paperjda.Listeners;
-
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
-public class DiscordListener extends ListenerAdapter {
-    // No events created yet.
-}
